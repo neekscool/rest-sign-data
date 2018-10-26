6695df5c13b8aeeaabeea422838883c5b41ca380
rt-heroku <rtorres@heroku.com>
2018-10-26 13:57:37 -0400

    refactor formatter

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RawRequestBodySample.Formatters;
 
 namespace restdotnet
 {
@@ -26,6 +27,10 @@ namespace restdotnet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options =>
+            {
+                options.InputFormatters.Insert(0, new RawRequestBodyInputFormatter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
